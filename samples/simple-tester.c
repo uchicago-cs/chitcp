3b190015f5ffe504530e67d1a6fb9a91b5fdb4cf
Borja Sotomayor <borja@cs.uchicago.edu>
2014-02-16 19:03:39 -0600

    Improved simple tester
    Now prints state changes and TCP variables

--- samples/simple-tester.c
+++ samples/simple-tester.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "chitcp/debug_api.h"
 #include "chitcp/tester.h"
+
+enum chitcpd_debug_response print_state_updates(int sockfd, enum chitcpd_debug_event event_flag, debug_socket_state_t *state_info, debug_socket_state_t *saved_state_info, int new_sockfd)
+{
+    /* If this is a passive socket, we also monitor any active sockets it spawns */
+    if (event_flag == DBG_EVT_PENDING_CONNECTION)
+    {
+        return DBG_RESP_ACCEPT_MONITOR;
+    }
+
+    if (event_flag == DBG_EVT_TCP_STATE_CHANGE)
+    {
+        printf("Socket %i: ", sockfd);
+        printf("[SND.UNA = %5i  SND.NXT = %5i  RCV.NXT = %5i]",
+                state_info->SND_UNA, state_info->SND_NXT, state_info->RCV_NXT);
+
+
+        if (saved_state_info)
+            printf(" %12s -> %12s\n",
+                    tcp_str(saved_state_info->tcp_state),
+                    tcp_str(state_info->tcp_state));
+        else
+            printf("              -> %12s\n", tcp_str(state_info->tcp_state));
+
+        chitcpd_debug_save_socket_state(state_info);
+    }
+
+    return DBG_RESP_NONE;
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +37,14 @@ int main(int argc, char *argv[])
 
     EXIT_ON_ERROR(chitcp_tester_init(&t),
             "Could not initialize tester");
+
+    EXIT_ON_ERROR(chitcp_tester_server_set_debug(&t, print_state_updates,
+            DBG_EVT_PENDING_CONNECTION| DBG_EVT_TCP_STATE_CHANGE),
+            "Could not set debug handler for client");
+
+    EXIT_ON_ERROR(chitcp_tester_client_set_debug(&t, print_state_updates,
+            DBG_EVT_TCP_STATE_CHANGE),
+            "Could not set debug handler for client");
 
     EXIT_ON_ERROR(chitcp_tester_start(&t),
             "Could not start tester");
@@ -21,7 +58,9 @@ int main(int argc, char *argv[])
     EXIT_ON_ERROR(chitcp_tester_client_connect(&t),
             "Tester client won't connect()");
 
-    getchar();
     chitcp_tester_client_close(&t);
     chitcp_tester_server_close(&t);
+
+    chitcp_tester_client_wait_for_state(&t, CLOSED);
+    chitcp_tester_server_wait_for_state(&t, CLOSED);
 }
