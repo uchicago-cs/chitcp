182d0807706d77fb176d6f5520aeb8fc46b518bb
Borja Sotomayor <borja@cs.uchicago.edu>
2019-01-28 19:04:13 -0600

    Added (currently empty) multitimer files

---
+++ samples/multitimer.c
@@ -0,0 +1,19 @@
+/*
+ *  chiTCP - A simple, testable TCP stack
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "chitcp/multitimer.h"
+
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
+
+
---
+++ tests/test_tcp_multitimer.c
@@ -0,0 +1,9 @@
+#include "chitcp/multitimer.h"
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <criterion/criterion.h>
+
+
