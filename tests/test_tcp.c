8f0edb5f4584b1fb483a72d5a3c7d3a29fc556cf
Borja Sotomayor <borja@cs.uchicago.edu>
2024-11-15 16:32:43 -0600

    Disable timeouts when running tests with Valgrind

--- tests/test_tcp.c
+++ tests/test_tcp.c
@@ -6,6 +6,19 @@
 #include <pthread.h>
 #include <signal.h>
 
+/*
+ * Checks whether the tests are running on Valgrind.
+ *  Source: https://stackoverflow.com/questions/365458/how-can-i-detect-if-a-program-is-running-from-within-valgrind
+ */
+int running_in_valgrind(void)
+{
+    char *p = getenv ("LD_PRELOAD");
+    if (p == NULL)
+        return 0;
+    return (strstr (p, "/valgrind/") != NULL ||
+            strstr (p, "/vgpreload") != NULL);
+}
+
 int main(int argc, char *argv[]) {
     struct criterion_test_set *tests = criterion_initialize();
 
@@ -25,21 +38,24 @@ int main(int argc, char *argv[]) {
     int result = 0;
     if (criterion_handle_args(argc, argv, true))
     {
-        if (criterion_options.debug != CR_DBG_NONE)
+        if (criterion_options.debug != CR_DBG_NONE || running_in_valgrind())
         {
-            /* When running in debug mode, disable all timeouts.
-               Otherwise, tests will time out while debugging.
-               (apparently using criterion_options.timeout has
+            /* When running in debug mode or in Valgrind,
+               disable all timeouts. Otherwise, tests will time
+               out while debugging. On Valgrind, the timeout may
+               prevent other errors from being detected.
+
+               Apparently using criterion_options.timeout has
                no effect, because that's only meant to affect
                tests with no specified time out, so we have to
                iterate over every test and change its timeout. */
             FOREACH_SET(struct criterion_suite_set *s, tests->suites)
-            {
-                FOREACH_SET(struct criterion_test *test, s->tests)
-                {
-                    test->data->timeout = 0.0;
-                }
-            }
+                    {
+                        FOREACH_SET(struct criterion_test *test, s->tests)
+                                {
+                                    test->data->timeout = 0.0;
+                                }
+                    }
         }
         result = !criterion_run_all_tests(tests);
     }
